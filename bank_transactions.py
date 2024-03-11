6fe40390c86f573029dcc39e09a7cc0b9b993133
baruh-d <barudaniel6@gmail.com>
2024-03-12 02:22:36 +0300

    updated

--- bank_transactions.py
+++ bank_transactions.py
@@ -5,15 +5,24 @@ def solution(A, D):
     dates = D #dates list
     bal = 0 #initial balance at beginning of year
     fee = 5 #fee to be paid per month
-    for amount in transactions: #iterating through the amount balance in the transaction list
+    prev_month = None #setting the value of previous month to nothing and will need to be updated
+    transactions_in_month = 0 #counter for transactions in the current month
+    for amount, date in zip(transactions, dates): #iterating through the amount balance in the transaction list
         if amount < 0:
-            bal -= abs(amount) #if amount is negative then do amount balance - amount
-            for transactions in dates:
-                if bal < amount 
-                
-        elif amount >= 0:
-            bal += abs(amount) #if amount is positive then do amount balance + amoun
-        
+            bal -= abs(amount) #if amount is negative then do absolute amount balance - amount to subtract it is a card payment
+        else:
+            bal += amount #if amount is positive then do amount balance + amount to add an incoming transfer
+            transactions_in_month += 1 #increment the transaction count for the new month
+        if date.month != prev_month:
+            if transactions_in_month >= 3 and sum(transactions) >= 100:
+                pass #if results to true then do not deduct fee
+            else:
+                bal -= fee #deduuct the fee for the month
+            transactions_in_month = 0 #reseting the transaction count for the new month
+            prev_month = date.month #update previous month to current month
     return bal
     
     
+transactions = [100, 100, -10, -20, -30] 
+dates = [datetime(2020, 1, 1), datetime(2020, 2, 1), datetime(2020, 2, 11), datetime(2020, 2, 5), datetime(2020, 2, 8)]
+print(solution(transactions, dates))
